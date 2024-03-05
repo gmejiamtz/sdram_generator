e54bfe8e3e1a48e3180016e2cdedf8aef71302b9
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2024-03-05 02:05:28 +0000

    Created Param Class to minimize Class definitions

--- sdram_controller_gen/src/main/scala/fsm.scala
+++ sdram_controller_gen/src/main/scala/fsm.scala
@@ -21,22 +21,23 @@ case class SDRAMControllerParams(width: Int, num_read_channels: Int, num_write_c
     require(num_write_channels > 0)
     val read_channels = num_read_channels
     val write_channels = num_write_channels
+    val data_width = width
 }
 
-class SDRAMControllerIO(num_read_channels: Int, num_write_channels: Int, width: Int) extends Bundle {
+class SDRAMControllerIO(p: SDRAMControllerParams) extends Bundle {
     //require at least one read and one write channel
-    require(num_read_channels > 0)
-    require(num_write_channels > 0)
+    require(p.num_read_channels > 0)
+    require(p.num_write_channels > 0)
     //read channels
-    val read_addresses = Input(Vec(num_read_channels, UInt(width.W)))
-    val read_data = Output(Vec(num_read_channels, Decoupled(UInt(width.W))))
+    val read_addresses = Input(Vec(p.num_read_channels, UInt(p.data_width.W)))
+    val read_data = Output(Vec(p.num_read_channels, Decoupled(UInt(p.data_width.W))))
     //write channels
-    val write_addresses = Input(Vec(num_read_channels, UInt(width.W)))
-    val write_data = Output(Vec(num_read_channels, Flipped(Decoupled(UInt(width.W)))))
+    val write_addresses = Input(Vec(p.num_read_channels, UInt(p.data_width.W)))
+    val write_data = Output(Vec(p.num_read_channels, Flipped(Decoupled(UInt(p.data_width.W)))))
     //debug purposes
     val state_out = Output(ControllerState())
 }
 
-class SDRAMController extends Module{
-    val io = IO(new SDRAMController)
+class SDRAMController(p: SDRAMControllerParams) extends Module{
+    val io = IO(new SDRAMControllerIO(p))
 }
