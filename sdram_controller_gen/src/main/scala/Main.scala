0b371eb963f5865c19e15653c65f351323f6ed23
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2024-03-06 17:30:36 +0000

    Changed main print string to show target of prgram

--- sdram_controller_gen/src/main/scala/Main.scala
+++ sdram_controller_gen/src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 object Hello {
     def main(args: Array[String]) = {
-        println("Hello, world")
+        println("This program is aimed at taking in a json config and spitting out Verilog for an SDRAM controller...")
     }
 }
