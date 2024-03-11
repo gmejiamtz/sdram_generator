28d5ba36e9abc1a5ad06f668d933f88281135cfc
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2024-03-11 06:14:45 +0000

    Passed tests for cas latency of 1 and 2 for reads

--- sdram_controller_gen/src/test/scala/testbench.scala
+++ sdram_controller_gen/src/test/scala/testbench.scala
@@ -95,7 +95,7 @@ class SDRAMControllerTestBench extends AnyFreeSpec with ChiselScalatestTester {
     }
   }
 
-  "Test for read data validity" in {
+  "Test for read data validity for cas latency of 3" in {
     //wanted coded item 0000_0111_0000
     val burst_length = 0
     val burst_type = 0
@@ -206,4 +206,118 @@ class SDRAMControllerTestBench extends AnyFreeSpec with ChiselScalatestTester {
       //write done 
     }
   }
+
+  "Tests for read data validity with cas latency of 2" in {
+  //wanted coded item 0000_0111_0000
+    val burst_length = 0
+    val burst_type = 0
+    val cas_latency = 2
+    val opcode = 0
+    val write_burst = 0
+    val params = new SDRAMControllerParams(16,12,1,1,burst_length,burst_type,cas_latency,opcode,write_burst)
+    val init_cycle_time = (Duration(100, MICROSECONDS).toNanos.toInt /params.period.toFloat).ceil.toInt
+    val active_to_rw_delay = params.active_to_rw_delay
+    test(new SDRAMController(params)).withAnnotations(Seq(WriteVcdAnnotation)) {dut =>
+        dut.clock.setTimeout(0)
+        //let sdram initialize and program
+        for(cycle <- 0 until (init_cycle_time + 3)){
+            dut.io.state_out.expect(ControllerState.initialization)
+            dut.clock.step()
+        }
+        dut.io.state_out.expect(ControllerState.initialization)
+        dut.clock.step()
+        dut.io.state_out.expect(ControllerState.idle)
+        //ready for input
+        dut.io.read_row_addresses(0).poke(0.U)
+        dut.io.read_start(0).poke(true.B)
+        //send active command
+        expectActive(dut)
+        dut.clock.step()
+        dut.io.read_start(0).poke(false.B)
+        //expect active state
+        for(act_to_read <- 0 until active_to_rw_delay){
+          dut.io.state_out.expect(ControllerState.active)
+          //expect nops
+          expectNOPs(dut)
+          dut.clock.step()
+        }
+        //expect read command being sent and send in an address
+        dut.io.read_col_addresses(0).poke(10.U)
+        expectRead(dut) 
+        dut.io.sdram_control.address_bus.expect(10.U)
+        dut.clock.step()
+        dut.io.state_out.expect(ControllerState.reading)
+        //loop until cas latency is reached
+        for(time_in_read <- 0 until cas_latency){
+          dut.io.state_out.expect(ControllerState.reading) 
+          dut.io.read_data_valid(0).expect(false.B)
+          dut.clock.step()
+        }
+        //no clock step means this value is high on a transition
+        dut.io.read_data_valid(0).expect(true.B)
+        //expect precharge to end read
+        expectPrecharge(dut)
+        dut.clock.step()
+        dut.io.state_out.expect(ControllerState.idle)
+        //read done
+      }
+  }
+
+  "Tests for read data validity with cas latency of 1" in {
+  //wanted coded item 0000_0111_0000
+    val burst_length = 0
+    val burst_type = 0
+    val cas_latency = 1
+    val opcode = 0
+    val write_burst = 0
+    val params = new SDRAMControllerParams(16,12,1,1,burst_length,burst_type,cas_latency,opcode,write_burst)
+    val init_cycle_time = (Duration(100, MICROSECONDS).toNanos.toInt /params.period.toFloat).ceil.toInt
+    val active_to_rw_delay = params.active_to_rw_delay
+    test(new SDRAMController(params)).withAnnotations(Seq(WriteVcdAnnotation)) {dut =>
+        dut.clock.setTimeout(0)
+        //let sdram initialize and program
+        for(cycle <- 0 until (init_cycle_time + 3)){
+            dut.io.state_out.expect(ControllerState.initialization)
+            dut.clock.step()
+        }
+        dut.io.state_out.expect(ControllerState.initialization)
+        dut.clock.step()
+        dut.io.state_out.expect(ControllerState.idle)
+        //ready for input
+        dut.io.read_row_addresses(0).poke(0.U)
+        dut.io.read_start(0).poke(true.B)
+        //send active command
+        expectActive(dut)
+        dut.clock.step()
+        dut.io.read_start(0).poke(false.B)
+        //expect active state
+        for(act_to_read <- 0 until active_to_rw_delay){
+          dut.io.state_out.expect(ControllerState.active)
+          //expect nops
+          expectNOPs(dut)
+          dut.clock.step()
+        }
+        //expect read command being sent and send in an address
+        dut.io.read_col_addresses(0).poke(10.U)
+        expectRead(dut) 
+        dut.io.sdram_control.address_bus.expect(10.U)
+        dut.clock.step()
+        dut.io.state_out.expect(ControllerState.reading)
+        //loop until cas latency is reached
+        for(time_in_read <- 0 until cas_latency){
+          dut.io.state_out.expect(ControllerState.reading) 
+          dut.io.read_data_valid(0).expect(false.B)
+          dut.clock.step()
+        }
+        //no clock step means this value is high on a transition
+        dut.io.read_data_valid(0).expect(true.B)
+        //expect precharge to end read
+        expectPrecharge(dut)
+        dut.clock.step()
+        dut.io.state_out.expect(ControllerState.idle)
+        //read done
+      }
+  }
+
+
 }
